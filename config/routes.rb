25886293ed8d0e956d209df36a039ba35a6b1a22
Aaron Banewicz <aaron.banewicz@gmail.com>
2017-06-21 13:37:31 -0400

    add user functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'peaks#index'
   get '/:id' => 'peaks#show', as: :peak
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
