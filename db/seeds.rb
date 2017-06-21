b8e5ae0a1e52374e4f42149fb83d88b6ce2cabdb
Aaron Banewicz <aaron.banewicz@gmail.com>
2017-06-21 19:14:44 -0400

    add picture files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Peak.create(name: "Mt. Colden", elevation: 4715, image: "mt_colden.jpg")
-Peak.create(name: "Mt. Marcy", elevation: 5344, image: "mt_marcy.jpg")
+Peak.create(name: "Mount Marcy", elevation: 5344, image: "mt_marcy.jpg")
+Peak.create(name: "Algonquin Peak", elevation: 5114, image: "algonquin.jpg")
+Peak.create(name: "Mount Haystack", elevation: 4960, image: "mt_haystack.jpg")
+Peak.create(name: "Mount Skylight", elevation: 4920, image: "mt_skylight.jpg")
+Peak.create(name: "Whiteface Mountain", elevation: 4867, image: "whiteface.jpg")
+Peak.create(name: "Dix Mountain", elevation: 4857, image: "dix.jpg")
+Peak.create(name: "Gray Peak", elevation: 4840, image: "gray_peak.jpg")
+Peak.create(name: "Iroquois Peak", elevation: 4840, image: "iroquois_peak.jpg")
+Peak.create(name: "Basin Mountain", elevation: 4827, image: "basin.jpg")
+Peak.create(name: "Gothics", elevation: 4736, image: "gothics.jpg")
+Peak.create(name: "Mount Colden", elevation: 4714, image: "mt_colden.jpg")
+
+
+
