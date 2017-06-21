512d9099b342084a58f05759ac17f3cd9d042736
Aaron Banewicz <aaron.banewicz@gmail.com>
2017-06-21 12:54:38 -0400

    add restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  resources :peaks
   root 'peaks#index'
-  get '/:id' => 'peaks#show', as: :peak
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
