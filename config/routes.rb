69fb48b2197c97d3a877194c3b608d675457f4f9
Aaron Banewicz <aaron.banewicz@gmail.com>
2017-06-26 20:07:48 -0400

    add user page and climb model

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   end
   root 'peaks#index'
   devise_for :users
+  get '/users/:id' => 'users#show', as: :user
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
